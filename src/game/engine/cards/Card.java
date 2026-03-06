cac58b2c22bda9f42bbd93c01f54e6cb8ced3416
abdelrahman_655 <abdelrahmansaid655@gmail.com>
2026-03-07 00:20:27 +0200

    Update Card.java

--- src/game/engine/cards/Card.java
+++ src/game/engine/cards/Card.java
@@ -1,5 +1,12 @@
 package game.engine.cards;
 
 public abstract class Card {
-
+   String name;
+   String description;
+   int rarity;
+   boolean lucky;
+  public  Card(String name, String description, int rarity, boolean lucky){
+    
+    
+  }
 }
