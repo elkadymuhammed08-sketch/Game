0c3b7963f38d8eb1d675a7f71b003afa34d6b9a4
elkadymuhammed08-sketch <elkadymuhammed08@gmail.com>
2026-03-06 14:48:32 +0200

    Add new classes

---
+++ src/game/engine/monsters/MultiTasker.java
@@ -0,0 +1,12 @@
+package game.engine.monsters;
+
+import game.engine.Role;
+
+public class MultiTasker extends Monster{
+    private int normalSpeedTurns;
+
+    public MultiTasker(String name, String description, Role role, int energy){
+        super(name, description, role, energy);
+        this.normalSpeedTurns = 0;
+    }
+}
