218cb1a7f08c3f66d3d149275bc5ae62dd8cc028
elkadymuhammed08-sketch <elkadymuhammed08@gmail.com>
2026-03-06 11:03:24 +0200

    Add subclass

---
+++ src/game/engine/monsters/Dasher.java
@@ -0,0 +1,19 @@
+package game.engine.monsters;
+
+import game.engine.Role;
+
+public class Dasher extends Monster{
+    private int momentumTurns;
+
+    public Dasher(String name, String description, Role role, int energy){
+        super(name, description, role, energy);
+        this.momentumTurns = 0;
+    }
+    public int getMomentumTurns(){
+        return this.momentumTurns;
+    }
+    public void setMomentumTurns(int m){
+        this.momentumTurns = m;
+    }
+
+}
