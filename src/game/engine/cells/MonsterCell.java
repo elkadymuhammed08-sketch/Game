27aa93d40d6fbbd067f4d124e35992d76bab8d46
M7amd_19ayman <mohamedayman1034@gmail.com>
2026-03-07 01:42:07 +0200

    Update MonsterCell.java

--- src/game/engine/cells/MonsterCell.java
+++ src/game/engine/cells/MonsterCell.java
@@ -1,5 +1,13 @@
 package game.engine.cells;
+import game.engine.monsters.Monster;
 
 public class MonsterCell extends Cell{
+  private Monster cellMonster;
+  public Monster getCellMonster() {
+        return cellMonster;
+    }
+  public MonsterCell(String name, Monster cellMonster) {
+        super(name);
+        this.cellMonster = cellMonster;
 
 }
