dac17c4fae049a353e29d53f1f52970c149ad719
M7amd_19ayman <mohamedayman1034@gmail.com>
2026-03-07 02:04:17 +0200

    Update ContaminationSock.java

--- src/game/engine/cells/ContaminationSock.java
+++ src/game/engine/cells/ContaminationSock.java
@@ -2,7 +2,7 @@ package game.engine.cells;
 
 public class ContaminationSock extends TransportCell{
    public ContaminationSock(String name, int effect) {
-        super(name, -Math.abs(effect));   // to the int effect alaways negative
+        super(name, -Math.abs(effect));   // to be sure that the int effect alaways negative
     }
 
 }
