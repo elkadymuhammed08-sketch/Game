d27b01d912b90a8548d6bb3aee2d60be20f39806
M7amd_19ayman <mohamedayman1034@gmail.com>
2026-03-07 02:14:15 +0200

    Update Cell.java

--- src/game/engine/cells/Cell.java
+++ src/game/engine/cells/Cell.java
@@ -1,5 +1,20 @@
 package game.engine.cells;
 
 public class Cell {
+   private String name;       
+   private Monster monster;
+   public String getName() {
+        return name;
+   }
+   public Monster getMonster() {
+        return monster;
+   }
+   public void setMonster(Monster monster) {
+        this.monster = monster;
+    }
+   public Cell(String name) {
+        this.name = name;
+        this.monster = null;
+    }
 
 }
